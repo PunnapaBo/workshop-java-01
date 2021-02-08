86e1cba352740dacc73f3e20ece89abe3b250fdc
punnapa.bo <punnapa.bo@kbtg.tech>
2021-02-08 14:15:13 +0700

    test create_buffer_with_specified_size_5

--- src/test/java/CircularBufferTest.java
+++ src/test/java/CircularBufferTest.java
@@ -9,9 +9,18 @@ class CircularBufferTest {
     @DisplayName("buffer size must be 10")
     public void create_buffer_with_default_size(){
         CircularBuffer circularBuffer = new CircularBuffer();
-        circularBuffer.create(5);
+        circularBuffer.create();
         int size = circularBuffer.getSize();
 
         assertEquals(10,size);
+    }
+    @Test
+    @DisplayName("buffer size must be 5")
+    public void create_buffer_with_specified_size_5(){
+        CircularBuffer circularBuffer = new CircularBuffer();
+        circularBuffer.create(5);
+        int size = circularBuffer.getSize();
+
+        assertEquals(5,size);
     }
 }
