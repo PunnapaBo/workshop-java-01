514b0124fe71483f6b1e5eaa538c09845a944006
punnapa.bo <punnapa.bo@kbtg.tech>
2021-02-08 13:02:16 +0700

    second commit

--- src/main/java/CircularBuffer.java
+++ src/main/java/CircularBuffer.java
@@ -1,3 +1,37 @@
 public class CircularBuffer {
 
+
+    private String[] array;
+    private int capacity = 0;
+    private int size = 0;
+    private int writePointer = 0;
+    private int readPointer = 0;
+
+    public CircularBuffer(int capacity){
+        this.capacity = capacity;
+        array = new String[capacity];
+    }
+
+    public void create(){
+        create(10);
+    }
+    public void create(int capacity){
+        new CircularBuffer(capacity);
+    }
+
+    public void write(Object element)    {
+
+    }
+    public String read(){
+
+
+
+        return "";
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+
 }
